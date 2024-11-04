4f43164d2863f661de249497895c79321188430a
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-11-04 11:19:28 +0100

    Fixed Vs Code errors about Tailwind

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
 <template>
-  <div class="bg-black text-6xl">hello</div>
+  <div class="">hello</div>
 </template>
+
+<style>
+/* reset */
+* {
+  @apply p-0 m-0 box-border;
+}
+
+body {
+  @apply bg-black;
+}
+</style>
