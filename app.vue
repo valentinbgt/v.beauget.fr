28c890c7ce8a5695604e88b5df62397d09828ac4
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-11-04 11:51:48 +0100

    Added video mask

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="text-6xl font-title">HELLO</div>
+  <div class="relative">
+    <div class="video-mask w-screen h-screen">
+      <div class="bg-vlightblue w-screen h-screen">
+        <p class="font-title text-9xl">CECI EST UN TEST</p>
+      </div>
+    </div>
+    <div class="absolute top-0 bottom-0 left-0 right-0">content</div>
+  </div>
 </template>
 
 <style>
@@ -10,5 +17,13 @@
 
 body {
   @apply bg-vblack;
+}
+
+.video-mask {
+  -webkit-mask-image: url("/vmask.png");
+  mask-image: url("/vmask.png");
+  mask-repeat: no-repeat;
+  mask-size: cover;
+  mask-position: center;
 }
 </style>
