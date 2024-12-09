3f77a53c4a07d3ed94c81e87df0b264172ba28a1
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-12-09 08:36:02 +0100

    Created socials icons bar

--- app.vue
+++ app.vue
@@ -12,11 +12,32 @@
     </div>
     <div class="absolute">
       <div class="h-screen">
-        <div>
-          <ul>
-            <li>github</li>
-            <li>linkedin</li>
-            <li>cv</li>
+        <div class="w-screen flex flex-row-reverse">
+          <ul class="flex flex-row m-5 md:m-10 xl:m-16">
+            <li>
+              <a href="https://github.com/valentinbgt"
+                ><img
+                  class="w-10 md:w-14 m-2.5 md:m-4"
+                  src="/icons/github.svg"
+                  alt=""
+              /></a>
+            </li>
+            <li>
+              <a href="https://v.beauget.fr/linkedin"
+                ><img
+                  class="w-10 md:w-14 m-2.5 md:m-4"
+                  src="/icons/linkedin.svg"
+                  alt=""
+              /></a>
+            </li>
+            <li>
+              <a href="https://v.beauget.fr/cv"
+                ><img
+                  class="w-10 md:w-14 m-2.5 md:m-4"
+                  src="/icons/cv.svg"
+                  alt=""
+              /></a>
+            </li>
           </ul>
         </div>
         <div class="flex flex-col items-center w-10/12 mx-auto">
