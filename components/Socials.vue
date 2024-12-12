bf4a6e431725169e2d5e17abd47bcfd61056cb51
Valentin BEAUGET <valentin.beauget@gmail.com>
2024-12-12 13:13:00 +0100

    fix: target blank socials

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,30 +1,27 @@
 <template>
   <ul class="flex flex-row m-5 md:m-10 xl:m-16 min-w-80 justify-center">
     <li>
-      <a href="https://github.com/valentinbgt"
+      <a href="https://github.com/valentinbgt" target="_blank"
         ><img
           class="w-10 md:w-14 m-2.5 md:m-4 social-icon"
           src="/icons/github.svg"
           alt=""
-          target="_blank"
       /></a>
     </li>
     <li>
-      <a href="https://v.beauget.fr/linkedin"
+      <a href="https://v.beauget.fr/linkedin" target="_blank"
         ><img
           class="w-10 md:w-14 m-2.5 md:m-4 social-icon"
           src="/icons/linkedin.svg"
           alt=""
-          target="_blank"
       /></a>
     </li>
     <li>
-      <a href="/cv"
+      <a href="/cv" target="_blank"
         ><img
           class="w-10 md:w-14 m-2.5 md:m-4 social-icon"
           src="/icons/cv.svg"
           alt=""
-          target="_blank"
       /></a>
     </li>
   </ul>
