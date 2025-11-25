b84fe6bc0a264f694e0af280c081b8587278d270
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-11-25 19:15:10 +0100

    wip: Trying to fix from url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      formUrl: process.env.FORM_URL || "",
+      formUrl: "",
     },
   },
   app: {
